84656ad8d22c5ebf976041fc64df816b4963cb04
plsgivemeachane <100744636+plsgivemeachane@users.noreply.github.com>
2023-12-09 18:05:57 +0700

    Update app.py

--- src/web/app.py
+++ src/web/app.py
@@ -5,8 +5,7 @@ from logger import log
 from bardapi import BardCookies
 
 cookie_dict = {
-    "__Secure-1PSID": "dQizb195ybo1-3d0hl1vq2G3Ef86-3KAfSwQwyWlW6Sn5H-FjMMGXImZN1QCbHUbcoHZ7w.",
-    "__Secure-1PSIDTS": "sidts-CjEBPVxjSshve7oZ2z9UHXnwPrd-X3AbLFV1CmaGVvhUhakO2SaSoBT2addpCtpd2WoYEAA",
+    "__Secure-1PSID": "dQhvWFqsmqBZFXIj3awh3yYbM3GZJBM6tGY_HC2Yyk9xnmf7kKG8p_RdQxjaZjHabDAnBA.",
 }
 bard = BardCookies(cookie_dict=cookie_dict)
 
