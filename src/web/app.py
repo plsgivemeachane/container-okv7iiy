4fd85fd0345b1763ef4b9bae7fd7c7e2207ed1b0
plsgivemeachane <100744636+plsgivemeachane@users.noreply.github.com>
2023-12-09 10:31:20 +0700

    Update app.py

--- src/web/app.py
+++ src/web/app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 from db import get_db, close_db
 import sqlalchemy
 from logger import log
+from bardapi import BardCookies
+
+cookie_dict = {
+    "__Secure-1PSID": "dQizb195ybo1-3d0hl1vq2G3Ef86-3KAfSwQwyWlW6Sn5H-FjMMGXImZN1QCbHUbcoHZ7w.",
+    "__Secure-1PSIDTS": "sidts-CjEBPVxjSshve7oZ2z9UHXnwPrd-X3AbLFV1CmaGVvhUhakO2SaSoBT2addpCtpd2WoYEAA",
+}
+bard = BardCookies(cookie_dict=cookie_dict)
 
 app = Flask(__name__)
 app.teardown_appcontext(close_db)
@@ -9,7 +16,7 @@ app.teardown_appcontext(close_db)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return bard.get_answer("Hello Bard")['content']
 
 
 @app.route("/health")
